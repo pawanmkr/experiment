656e839177c196f9e710dd34dcbd54e652451d8e
“Pawan <pawan.kumar@carvach.com>
2024-09-04 15:10:37 +0530

    global prefix added

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    const globalPrefix = 'demo';
     app.enableCors({ origin: '*' });
+    app.setGlobalPrefix(globalPrefix);
 
     const config = new DocumentBuilder()
         .setTitle('Carvach Fleet Demo API')
         .setVersion('1.0')
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api', app, document, {
+        useGlobalPrefix: true,
+    });
 
     await app.listen(8000);
 }
